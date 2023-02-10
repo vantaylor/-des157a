5c96141283bf851faacfbf44c92699865faeb73d
Taylor Van <tvan22@scc2102-22.ucdavis.edu>
2023-02-09 16:46:27 -0800

    mad libs studio update

---
+++ studio1/script.js
@@ -0,0 +1,15 @@
+( function(){
+    'use strict';
+    console.log('running js');
+
+    const openBtns = document.querySelectorAll('#send-form');
+
+    for (const eachBtn of openBtns){
+        eachBtn.addEventListener('click', function(event){
+             event.preventDefault();
+             const thisBtn = event.target.id;
+             document.getElementById(`message${thisBtn}`).className = 'overlay showing';
+         });
+     }
+    
+} )();

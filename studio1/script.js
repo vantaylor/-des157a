3d5e66890a78eefc9a979563536aaf8536f0bce5
Taylor Van <tavan@ucdavis.edu>
2023-02-14 03:39:48 -0800

    madlib js

--- studio1/script.js
+++ studio1/script.js
@@ -2,14 +2,31 @@
     'use strict';
     console.log('running js');
 
-    const openBtns = getElementById('#send-form');
+    const form = document.querySelector('#myform');
+    const madlib = document.querySelector('#madlib');
 
-    for (const eachBtn of openBtns){
-        eachBtn.addEventListener('click', function(event){
-             event.preventDefault();
-             const thisBtn = event.target.id;
-             document.getElementById(`message${thisBtn}`).className = 'overlay showing';
-         });
-     }
+    form.addEventListener('submit', function(event){
+
+        event.preventDefault();
+
+        const name = document.querySelector('#name').value;
+        const adj1 = document.querySelector('#adj1').value;
+        const adj2 = document.querySelector('#adj2').value;
+        const exclamation = document.querySelector('#exclamation').value;
+        const noun = document.querySelector('#noun').value;
+        const verb = document.querySelector('#verb').value;
+
+        let myText;
+
+        if (name == ''){
+            myText = 'Uh oh! Santa needs your name on the list!';
+            document.querySelector('#name').focus();
+        }
+        else if (adj1 == ''){
+            myText = 'Please enter an adjective'
+        }
     
+    });
+        
+
 } )();

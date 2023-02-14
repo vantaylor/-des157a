ddce89c1d6b3cf6b966fb09b98cac5caa288f1d4
Taylor Van <tavan@ucdavis.edu>
2023-02-14 12:43:51 -0800

    madlib updates

--- studio1/script.js
+++ studio1/script.js
@@ -58,7 +58,7 @@
             document.querySelector('#verb').value;
         }
         else {
-            myText = `Merry Christmas ${name}! Welcome to Santa's Top Secret Workshop. Oh you look ${adj1}! Did you happen to meet ${petName}, the ${color} ${bodyPart} Reindeer`;
+            myText = `Merry Christmas ${name}! You'll never guess what happened! I snuck into Santa's Top Secret Workshop. It was ${adj1}! Now, I just need to find Santa. While I was exploring, I met ${petName}, the ${color} ${bodyPart} Reindeer. That ${adj2} reindeer almost alerted the Elves! ${exclamation} Luckily, I was able to calm ${petName} down with some cookies I nabbed from Mrs. Claus's kitchen. I found ${noun} in the Top Secret Workshop. I wish you came with me, then we can ${verb} together. Gotta go! I still need to find Santa. I'll keep you updated!`;
         }
         
 

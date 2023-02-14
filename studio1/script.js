486cde85773197001960040f7cea1e9dedb6488e
Taylor Van <tavan@ucdavis.edu>
2023-02-14 11:49:37 -0800

    more madlib

--- studio1/script.js
+++ studio1/script.js
@@ -12,6 +12,9 @@
         const name = document.querySelector('#name').value;
         const adj1 = document.querySelector('#adj1').value;
         const adj2 = document.querySelector('#adj2').value;
+        const petName = document.querySelector('#petName').value;
+        const color = document.querySelector('#color').value;
+        const bodyPart = document.querySelector('#body').value;
         const exclamation = document.querySelector('#exclamation').value;
         const noun = document.querySelector('#noun').value;
         const verb = document.querySelector('#verb').value;
@@ -23,8 +26,43 @@
             document.querySelector('#name').focus();
         }
         else if (adj1 == ''){
-            myText = 'Please enter an adjective'
+            myText = 'Please enter an adjective';
+            document.querySelector('#adj1').value;
         }
+        else if (petName == ''){
+            myText = 'Please enter a funny pet name';
+            document.querySelector('#petName').value;
+        }
+        else if (color == ''){
+            myText = 'Please enter a color';
+            document.querySelector('#color').value;
+        }
+        else if (bodyPart == ''){
+            myText = 'Please enter a funny pet name';
+            document.querySelector('#body').value;
+        }
+        else if (adj2 == ''){
+            myText = 'Please enter an adjective';
+            document.querySelector('#adj2').value;
+        }
+        else if (exclamation == ''){
+            myText = 'Please enter an exclamation';
+            document.querySelector('#exclamation').value;
+        }
+        else if (noun == ''){
+            myText = 'Please enter a noun';
+            document.querySelector('#noun').value;
+        }
+        else if (verb == ''){
+            myText = 'Please enter a verb';
+            document.querySelector('#verb').value;
+        }
+        else {
+            myText = `Merry Christmas ${name}! Welcome to Santa's Top Secret Workshop. Oh you look ${adj1}! Did you happen to meet ${petName}, the ${color} ${bodyPart} Reindeer`;
+        }
+        
+
+        madlib.innerHTML = myText;
     
     });
         

1b701a05853fc958c42c9acd3ede957dddd1e083
Taylor Van <tvan22@scc2102-19.ucdavis.edu>
2023-03-02 14:40:49 -0800

    game on start

---
+++ studio3/script.js
@@ -0,0 +1,98 @@
+( function(){
+    'use scrict';
+    console.log('js running');
+
+    const startGame = document.getElementById('startgame');
+    const gameControl = document.getElementById('gamecontrol');
+    const game = document.getElementById('game');
+    const score = document.getElementById('score');
+    const actionArea = document.getElementById('actions');
+    
+    const gameData = {
+        dice: ['1die.jpg', '2die.jpg', '3die.jpg', '4die.jpg', '5die.jpg', '6die.jpg'],
+        players: ['player1', 'player2'],
+        score: [0, 0],
+        roll1: 0,
+        roll2: 0,
+        rollSum: 0,
+        index: 0,
+        gameEnd: 29
+    }
+
+    startGame.addEventListener('click', function(){
+        gameData.index = Math.round(Math.random());
+        gameControl.innerHTML = '<h2>The game has started</h2>';
+        gameControl.innerHTML += '<button id="quit">Wanna Quit?</button>';
+        document.getElementById('quit').addEventListener('click', function(){
+            location.reload();
+        });
+        //console.log(gameData.index);
+        //console.log('set up the turn');
+        setUpTurn();
+    });
+
+    function setUpTurn(){
+        game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
+        actionArea.innerHTML = '<button id="roll">Roll the Dice</button>';
+        document.getElementById('roll').addEventListener('click', function(){
+            //console.log('roll the dice!');
+            throwDice();
+        });
+    }
+
+    function throwDice(){
+        actionArea.innerHTML = '';
+        gameData.roll1 = Math.floor(Math.random() * 6) + 1;
+        gameData.roll2 = Math.floor(Math.random() * 6) + 1;
+        game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
+        game.innerHTML += `<img src="${gameData.dice[gameData.roll1-1]}">`;
+        game.innerHTML += `<img src="${gameData.dice[gameData.roll2-1]}">`;
+        gameData.rollSum = gameData.roll1 + gameData.roll2;
+
+        if ( gameData.rollSum == 2 ){
+            game.innerHTML += '<pOh snap! Snake Eyes!</p>';
+            gameData.score[gameData.index] = 0;
+            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+            setTimeout(setUpTurn, 2000);
+            showCurrentScore();
+        }
+        else if(gameData.roll1 === 1 || gameData.roll2 === 1){
+            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+            game.innerHTML += `<p>Sorry, one of your rolls was a one. Switching to ${gameData.players[gameData.index]}</p>`;
+            setTimeout(setUpTurn, 2000);
+        }
+        else{
+            gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
+            actionArea.innerHTML = '<button id="rollagain">Roll Again</button> <button id="pass">Pass</button>';
+
+            document.getElementById('rollagain').addEventListener('click', function(){
+                setUpTurn();
+            });
+
+            document.getElementById('pass').addEventListener('click', function(){
+                gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+                setUpTurn();
+            });
+
+            checkWinningCondition();
+        }
+    }
+
+    function checkWinningCondition(){
+        if( gameData.score[gameData.index] > gameData.gameEnd ){
+            score.innerHTML = `<h2>${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!</h2>`;
+
+            actionArea.innerHTML = '';
+            document.getElementById('quit').innerHTML = "Start a New Game";
+        }
+        else {
+            showCurrentScore();
+        }
+    }
+
+    function showCurrentScore(){
+        score.innerHTML = `<p>The score is currently <strong>${gameData.players[0]} ${gameData.score[0]}</strong> and <strong>${gameData.players[0]} ${gameData.score[1]}</strong></p>`;
+    }
+
+
+} )();

8ff4d21dcc7530a385fe92b6cf87185a49f12efb
Taylor Van <tavan@ucdavis.edu>
2023-03-23 21:18:47 -0700

    more updates

---
+++ final/version1/script.js
@@ -0,0 +1,26 @@
+( function(){
+    'use strict';
+    console.log('js running');
+
+    /*define variables to activate pop-up modal*/
+    const openBtns = document.querySelectorAll('.open');
+    const closeBtns = document.querySelectorAll('.close');
+
+    /*opens pop-up modal when user clicks on images*/
+    for (const eachBtn of openBtns){
+       eachBtn.addEventListener('click', function(evt){
+            evt.preventDefault();
+            const thisBtn = evt.target.id;
+            document.getElementById(`product-${thisBtn}`).className = 'overlay showing';
+        });
+    }
+
+    /*deactivated modal and closes it when button is clicked*/
+    for (const eachBtn of closeBtns){
+        eachBtn.addEventListener('click', function(evt){
+            evt.preventDefault();
+            document.querySelector('.showing').className = 'overlay hidden';
+        });
+    }
+
+} )();
